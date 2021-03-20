447e2b5f72bf11bb57c9d137ab825d3b11afa1eb
Nnamdi Okeke <nahorr@gmail.com>
2021-03-20 16:47:28 -0600

    publish

--- src/GiphyServiceProvider.php
+++ src/GiphyServiceProvider.php
@@ -15,6 +15,7 @@ class GiphyServiceProvider extends ServiceProvider
     {
         // register our controller
         $this->app->make('Nahorr\Giphy\GiphyController');
+        $this->loadViewsFrom(__DIR__.'/views', 'giphy');
 
 
     }
@@ -28,13 +29,18 @@ class GiphyServiceProvider extends ServiceProvider
     {
        //Register Routes
        include __DIR__.'/routes.php';
-       $this->loadViewsFrom(__DIR__.'/views', 'giphy');
-       $this->publishes([
+
+       /* $this->publishes([
 
             // Views
             __DIR__.'/../resources/views/search.blade.php' => resource_path('views/vendor/nnamdi/search.blade.php'),
 
-        ]);
+        ]); */
+
+        $this->publishes([
+            __DIR__.'/../views/' => base_path('resources/views/vendor/giphy'),
+
+        ], 'assets');
 
 
     // Translations
